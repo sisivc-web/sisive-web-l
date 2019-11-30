c5dca2f3489dc36278fd50698eda12fc41c35696
liujianqiang <liujianqiangnew@gmail.com>
2019-11-30 20:32:56 +0800

    update news

---
+++ src/components/commonComponents/news-item.vue
@@ -0,0 +1,78 @@
+<template>
+    <div style="border-bottom: 1px solid #F5F5F5;">
+        <el-card :body-style="{ padding: '0px' }" class="news-item">
+            <div>
+                <div style="float: left; width: 25%;">
+                    <a href="javascript:;" @click="_gotoDetails(item.id)" class="animation hover-animation">
+                        <img :src="item.image" class="image" style="width:100%;height:auto">
+                    </a>
+                </div>
+            
+            <div style="float: left; width: 75%; padding: 10px 0px 10px 100px;" @click="_gotoDetails(item.id)">
+                <div style="width:100%; height:25px;">
+                    <span style="float:left; color: #222;font-weight: bold;font-size: 16px;">{{item.title}}</span>
+                    <span style="float:right; color: #a0a0a1;font-size: 14px;">{{item.createOn}}</span>
+                </div>
+                <div style="width: 80%; height: 70px; overflow: hidden;">
+                    <p style="font-size:13px; line-height:18px;">{{item.content}}</p>
+                </div>
+                <el-button type="text" class="button" style="font-size:14px;color:#a0a0a1; float:right; ">阅读全文</el-button>
+            </div>
+            </div>
+            
+        </el-card>  
+    </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    item: {},
+  },
+  data() {
+    return {
+        userIfo: {},
+    }
+  },
+  methods: { 
+    _gotoDetails(newsId) {
+      this.$router.push({path: '/newsDetail', query: {newsId: newsId}})
+    },
+  }
+}
+</script>
+<style lang="stylus" scoped rel="stylesheet/stylus">
+@import '~common/stylus/variable'
+@import '~common/stylus/base'
+@import "~common/stylus/mixin"
+.el-card.news-item
+    padding-left: 100px
+    padding-right: 100px
+    border: none
+    border-radius: 0
+    box-shadow: none
+    a.animation
+      line-height: 0;
+      img
+        -webkit-transition: -webkit-transform 1s;
+        -moz-transition: -moz-transform 1s;
+        -o-transition: -o-transform 1s;
+        transition: transform 1s;
+    a.hover-animation
+      overflow: hidden;
+      display: block;
+    a.hover-animation:hover img
+      -webkit-transform: scale(1.05);
+      -moz-transform: scale(1.05);
+      -ms-transform: scale(1.05);
+      -o-transform: scale(1.05);
+      transform: scale(1.05);
+    .operate
+        @extend .flex
+        @extend .flex-row
+        @extend .flex-between
+        .button
+            color: #808080
+            font-size: 12px
+            margin: 0
+            text-align: right
+</style>
--- src/components/news/news.vue
+++ src/components/news/news.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
     <m-anchor :contentDivClass="'AboutGame'" :anchorItem="'anchor-item'" :subNavList="[$t('subNavs.aboutUs1'), $t('subNavs.aboutUs2'), $t('subNavs.aboutUs3'), $t('subNavs.aboutUs4'), $t('subNavs.aboutUs5')]"></m-anchor>  
+    <div style="height:80px; padding-left: 100px; padding-top:30px; border-bottom: 1px solid #F5F5F5;">
+      <span style="font-size:25px; font-weight: bold;">新闻</span>
+    </div>
     <div>
-      <el-card :body-style="{ padding: '0px' }" class="member-item" v-for="(item, index) in data" :key="index">
-            <a href="javascript:;" @click="_gotoDetails(index)" class="animation hover-animation">
-                <img src="static/image/sisivc/award/0.jpg" class="image" style="width:100%;height:auto">
-            </a>
+      <el-card :body-style="{ padding: '0px' }" v-for="(item, index) in newsList" :key="index" class="news-item">
+            <news-item :item="item"></news-item>
         </el-card>  
     </div>
   </div>
@@ -14,17 +15,19 @@
 
 import { kpiNews }from 'apx'
 import MAnchor from 'components/m-anchor/m-anchor'
+import NewsItem from 'components/commonComponents/news-item'
 export default {
   components: {
-    MAnchor
+    MAnchor,
+    NewsItem
   },
   created() {
     // this._getURLQuery()
-    // this._getData()
+    this._getData()
   },
   data() {
     return {
-      data: [1,2,3],
+      newsList: [1,2,3],
       year: '2019'
     }
   },
@@ -44,7 +47,7 @@ export default {
         language: JSON.parse(window.localStorage.getItem('immi_language'))
       }
       kpiNews(param, this).then((res) => {
-        this.data = res.data.data
+        this.newsList = res.data.data
       })
     },
   }
@@ -53,7 +56,10 @@ export default {
 <style lang="stylus" scoped rel="stylesheet/stylus">
 @import "~common/stylus/base"
 @import "~common/stylus/variable"
-
+.el-card.news-item
+  border-radius: 0
+  border: none
+  box-shadow: none
 @media (min-width: 1920px)  {
   .container .other .block {
     width: 1560px;
