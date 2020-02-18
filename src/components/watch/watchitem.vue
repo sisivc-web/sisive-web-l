48f159deaac0d743c96bccbccebd8935e80c5054
gaojuyan <gmailsky@qq.com>
2020-02-18 14:22:33 +0800

    update style

--- src/components/watch/watchitem.vue
+++ src/components/watch/watchitem.vue
@@ -2,14 +2,14 @@
   <div style="position:relative;">
     <div style="padding:0px 10px;min-height:220px;max-height:300px;overflow:hidden;overflow-y:auto !important;">
       <div v-for="player in watchData.players" :key="player.id">
-        <div style="margin: 12px 0;color:white;" class="flex-between">
+        <div style="margin: 12px 0;color:white;font-weight:bold;" class="flex-between">
           <div>{{player.userName}}</div>
           <div>{{player.nationality}}</div>
           <div v-show="currentEye !== 'player'+player.id"><i class="el-icon-view" @click="currentEye='player'+player.id"></i></div>
           <img v-show="currentEye == 'player'+player.id" :src="player.image" style="width:44px;"/>
         </div>
         <div :style="{display: currentEye == 'player'+player.id ? 'block' : 'none'}">
-          <div style="color:white;color:#F0F0F0;font-size:10px;line-height:15px;" v-for="(vol,key) in player.playerChapterVoList" :key="key">
+          <div style="color:white;font-weight:bold;opacity:0.7;;font-size:10px;line-height:15px;" v-for="(vol,key) in player.playerChapterVoList" :key="key">
             <div style="margin:5px 0;">{{vol.composers}}:</div>
             <div style="margin:5px 0;">{{vol.repertoire}},{{vol.chapters}}</div>
           </div>
@@ -17,7 +17,7 @@
       </div>
       <div style="padding:0px 0px;"><divider :height="1"></divider></div>
       <div style="padding:0px 0px;" v-for="artist in watchData.artists" :key="artist.id">
-        <div style="margin: 12px 0;color:white;" class="flex-between">
+        <div style="margin: 12px 0;color:white;font-weight:bold;" class="flex-between">
           <div>{{artist.userName}}</div>
           <div>{{artist.nationality}}</div>
           <div v-show="currentEye !== 'artist'+artist.id"><i class="el-icon-view" @click="currentEye='artist'+artist.id"></i></div>
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div style="padding:0px 0px;" v-for="group in watchData.groups" :key="group.id">
-        <div style="margin: 12px 0;color:white;" class="flex-between">
+        <div style="margin: 12px 0;color:white;font-weight:bold;" class="flex-between">
           <span>{{group.orchestraTitle}}</span>
           <div>{{group.nationality}}</div>
           <span>{{group.type || '乐团'}}</span>
@@ -59,8 +59,6 @@
     },
     data() {
       return {
-        watchData: '',
-        themeColor: '',
         currentEye: ''
       }
     },
